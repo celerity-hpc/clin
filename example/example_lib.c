fb5ae6d629a6c4cde3568788f8fe0250bb6eb6d4
Philip Salzmann <philip.salzmann@uibk.ac.at>
2018-07-10 14:43:34 +0200

    Make logging behavior configurable
    Logging can now be turned off, or written to stdout instead of a file.
    
    This also updates the example library to showcase the new macro options, and
    also to demonstrate how a custom intercepted method can be defined.

--- example/example_lib.c
+++ example/example_lib.c
@@ -1,2 +1,12 @@
+#define CLIN_ENABLE_LOGGING 1
+#define CLIN_LOG_TO_FILE 0
+
+#define CLIN_CUSTOM_clGetPlatformIDs
+
 #include "clin.inc"
+
+CLIN_ENTRY cl_int CL_API_CALL clGetPlatformIDs(cl_uint num_entries, cl_platform_id* platforms, cl_uint* num_platforms) {
+    CLIN_LOG("###### INTERCEPTED: clGetPlatformIDs");
+    return clin_o_clGetPlatformIDs(num_entries, platforms, num_platforms);
+}
 
--- logging.h
+++ logging.h
@@ -1,16 +1,42 @@
 #include <stdio.h>
 
-#define CLIN_LOG(_STR, ...) \
+#ifndef CLIN_ENABLE_LOGGING
+#define CLIN_ENABLE_LOGGING 0
+#endif
+
+#ifndef CLIN_LOG_TO_FILE
+#define CLIN_LOG_TO_FILE 0
+#endif
+
+#if CLIN_ENABLE_LOGGING
+#define CLIN_LOG(_STR, ...) _CLIN_LOG(_STR, ## __VA_ARGS__)
+#else
+#define CLIN_LOG(_STR, ...)
+#endif
+
+#if CLIN_LOG_TO_FILE
+
+#define _CLIN_LOG(_STR, ...) \
 fprintf(clin_g_log_file, "%10.3f | " _STR "\n", clin_get_timestamp(), ## __VA_ARGS__);
 
-#define CLIN_LOG_NAME "clin.log"
+#else
 
+#define _CLIN_LOG(_STR, ...) \
+printf("%10.3f | " _STR "\n", clin_get_timestamp(), ## __VA_ARGS__);
+
+#endif // CLIN_LOG_TO_FILE
+
+#define CLIN_LOG_NAME "clin.log"
 FILE* clin_g_log_file;
 
 void clin_logging_init() {
+#if CLIN_LOG_TO_FILE
 	clin_g_log_file = fopen("clin.log", "w+");
+#endif
 }
 
 void clin_logging_end() {
+#if CLIN_LOG_TO_FILE
 	fclose(clin_g_log_file);
+#endif
 }
